19b03925cb7c45056c16b5608de969bb7752c924
Gaurav Kumar <gauravecb09@gmail.com>
2020-09-17 11:08:22 +0000

    Version 1.0

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,7 +4,7 @@ sap.ui.define([
 	"sap/ui/table/RowActionItem",
 	"sap/ui/table/RowSettings",
 	"sap/m/MessageToast"
-], function (Controller, RowAction, RowActionItem, RowSettings,MessageToast) {
+], function (Controller, RowAction, RowActionItem, RowSettings, MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.dynamiccolumn.DynamicColumn.controller.View1", {
@@ -15,7 +15,7 @@ sap.ui.define([
 				columnName: "lastName"
 			}, {
 				columnName: "department"
-			},{
+			}, {
 				columnName: "active"
 			}];
 
@@ -37,48 +37,67 @@ sap.ui.define([
 			}];
 			var oTable = this.getView().byId("idTable");
 			var oModel = new sap.ui.model.json.JSONModel();
-		oModel.setData({
-		    rows: rowData,
-		    columns: columnData
-		});
-		oTable.setModel(oModel);
-      
-		oTable.bindColumns("/columns", function(sId, oContext) {
-			  var columnName = oContext.getObject().columnName;
-				var oTemplate =  new sap.m.ObjectStatus({
-					text : "{"+ columnName +"}",
-		        	state: {path : columnName , formatter : function(data){
-		        		if(data){
-		        			if(data == "Retired"){
-		        				return "Error";
-		        			} else if (data == "Active") {
-		        				return "Success";
-		        			} else {
-		        				return "None";
-		        			}
-		        			
-		        		}
-		        	
-		        	}}
-				});
-		
+			oModel.setData({
+				rows: rowData,
+				columns: columnData
+			});
+			oTable.setModel(oModel);
 
-		    return new sap.ui.table.Column({
-		        label: columnName,
-		        filterProperty: columnName, 
-		        sortProperty : columnName,
-		        template: oTemplate
-		    });
-		});
-      
-	
-	
-	    	oTable.bindRows("/rows");
+			oTable.bindColumns("/columns", function (sId, oContext) {
+				var columnName = oContext.getObject().columnName;
+				var oTemplate = new sap.m.ObjectStatus({
+					text: "{" + columnName + "}",
+					state: {
+						path: columnName,
+						formatter: function (data) {
+							if (data) {
+								if (data == "Retired") {
+									return "Error";
+								} else if (data == "Active") {
+									return "Success";
+								} else {
+									return "None";
+								}
+
+							}
+
+						}
+					}
+				});
+
+				return new sap.ui.table.Column({
+					label: columnName,
+					filterProperty: columnName,
+					sortProperty: columnName,
+					template: oTemplate
+				});
+			});
+
+			oTable.bindRows("/rows");
 		},
-		handleActionPress : function(oEvent) {
+		handleActionPress: function (oEvent) {
 			var oRow = oEvent.getParameter("row");
 			var oItem = oEvent.getParameter("item");
 			var sSelectedObject = oItem.getBindingContext().getObject();
-		} 
+
+			var oView = this.getView();
+			
+			if (!this.byId("edit")) {
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "com.dynamiccolumn.DynamicColumn.Fragment.Edit",
+					controller: this
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				this.byId("edit").open();
+			}
+		},
+
+	
 	});
 });
